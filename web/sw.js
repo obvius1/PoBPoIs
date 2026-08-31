08b2f018988683409aa5b2b26a601e3adf045011
Lauren Schouppe <lauren.schouppe@profility.be>
2026-08-31 14:56:59 +0200

    update on 31/08

--- web/sw.js
+++ web/sw.js
@@ -7,7 +7,7 @@
  *              De app controleert eerst of we op WiFi zitten.
  */
 
-const CACHE_VERSION = 'v17';
+const CACHE_VERSION = 'build-966f1e1e';
 const CACHE_NAME = `peaks-pois-${CACHE_VERSION}`;
 
 // App shell: klein, altijd gecached, ook op mobiele data
